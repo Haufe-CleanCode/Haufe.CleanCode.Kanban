0d1ad372bfec8d55bbe6a3a55d3d74924e6af450
Pfaadt, Jochen <Jochen.Pfaadt@haufe-lexware.com>
2021-07-16 16:31:53 +0200

    Implement CreateBoard()

---
+++ kanbanboard/kanbanboard.kanban/BoardFactory.cs
@@ -0,0 +1,42 @@
+﻿using kanbanboard.contracts;
+using System.Collections.Generic;
+
+namespace kanbanboard.kanban
+{
+	public class BoardFactory : IBoardFactory
+	{
+		public Board CreateBoard(BoardConfig boardConfig, List<List<Item>> items)
+		{
+			var board = new Board();
+			
+			for(int i = 0; i< boardConfig.Columns.Count; i++)
+			{
+				AddColumn(board);
+
+				CopyHeader(boardConfig, board, i);
+
+				CopyItems(items, board, i);
+			}
+
+			return board;
+		}
+
+		private static void AddColumn(Board board)
+		{
+			board.Columns.Add(new Column());
+		}
+
+		private static void CopyItems(List<List<Item>> items, Board board, int i)
+		{
+			foreach (Item item in items[i])
+			{
+				board.Columns[i].Items.Add(item);
+			}
+		}
+
+		static void CopyHeader(BoardConfig boardConfig, Board board, int i)
+		{
+			board.Columns[i].Header = boardConfig.Columns[i].Header;
+		}
+	}
+}
