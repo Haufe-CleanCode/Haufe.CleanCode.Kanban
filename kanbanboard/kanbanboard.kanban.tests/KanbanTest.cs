d2c3e39863113b4360367093518931eeb854549d
Pfaadt, Jochen <Jochen.Pfaadt@haufe-lexware.com>
2021-07-16 16:43:01 +0200

    Add IdGenerator test

--- kanbanboard/kanbanboard.kanban.tests/KanbanTest.cs
+++ kanbanboard/kanbanboard.kanban.tests/KanbanTest.cs
@@ -49,5 +49,25 @@ namespace kanbanboard.kanban.tests
 			Assert.That(board.Columns[0].Items[0].Id, Is.EqualTo("id"));
 			Assert.That(board.Columns[0].Items[0].Text, Is.EqualTo("text"));
 		}
+
+		[Test]
+		public void IdGenerator()
+		{
+			var x = new IdGenerator();
+
+			var guid = x.CreateId();
+
+			var guidPart = guid.Split("-");
+
+			//check for format 8-4-4-4-12
+			Assert.That(guidPart.Length, Is.EqualTo(5));
+
+			Assert.That(guidPart[0].Length, Is.EqualTo(8));
+			Assert.That(guidPart[1].Length, Is.EqualTo(4));
+			Assert.That(guidPart[2].Length, Is.EqualTo(4));
+			Assert.That(guidPart[3].Length, Is.EqualTo(4));
+			Assert.That(guidPart[4].Length, Is.EqualTo(12));
+
+		}
 	}
 }
