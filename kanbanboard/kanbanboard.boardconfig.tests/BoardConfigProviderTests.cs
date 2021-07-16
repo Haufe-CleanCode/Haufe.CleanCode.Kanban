c883cae8cfe69d3bd55fb1b3ff8e8678b0b04cbd
IngoFroehlich <87530095+IngoFroehlich@users.noreply.github.com>
2021-07-16 16:12:46 +0200

    Add BoardConfigProvider

---
+++ kanbanboard/kanbanboard.boardconfig.tests/BoardConfigProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using kanbanboard.boardconfig;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kanbanboard.boardconfig.Tests
+{
+    [TestFixture()]
+    public class BoardConfigProviderTests
+    {
+        [Test()]
+        public void LoadBoardConfigTest()
+        {
+            var boardConfigProvider = new BoardConfigProvider();
+            var content = boardConfigProvider.LoadBoardConfig();
+            var contentHeader = content.Columns.Select(h => h.Header).ToList();
+            var contentWip = content.Columns.Select(w => w.WIPLimit).ToList();
+            var expectedHeader = new List<string>() { "To do", "In progress", "Done" };
+            var expectedWip = new List<int>() {0, 3, 0};
+            Assert.Multiple(() =>
+                {
+                    Assert.That(contentHeader, Is.EqualTo(expectedHeader));
+                    Assert.That(contentWip, Is.EqualTo(expectedWip));
+                }
+            );
+        }
+    }
+}
---
+++ kanbanboard/kanbanboard.boardconfig/BoardConfigProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using kanbanboard.contracts;
+
+namespace kanbanboard.boardconfig
+{
+    public class BoardConfigProvider : IBoardConfigProvider
+    {
+        public BoardConfig LoadBoardConfig()
+        {
+            var boardConfig = new BoardConfig();
+            var fileContent = File.ReadAllLines("Board.Config");
+            foreach(var line in fileContent)
+            {
+                var newColumn = new BoardConfigColumn();
+                if (line.Contains(";"))
+                {
+                    var semikolonPositionIndex = line.IndexOf(";", StringComparison.CurrentCulture);
+                    newColumn.Header = line.Substring(0, semikolonPositionIndex);
+                    var wip = line.Substring(semikolonPositionIndex + 1, line.Length - semikolonPositionIndex - 1);
+                    newColumn.WIPLimit = int.Parse(wip);
+                }
+                else
+                {
+                    newColumn.Header = line;
+                }
+                boardConfig.Columns.Add(newColumn);
+            }
+
+            return boardConfig;
+        }
+    }
+}
